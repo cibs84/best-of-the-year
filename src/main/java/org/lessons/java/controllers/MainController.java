4431db1bd366c0a3e1dbb892d30f53e3c9be22fe
cibs84 <d.tilesi@gmail.com>
2023-02-10 19:20:13 +0100

    Step 2 completato

--- src/main/java/org/lessons/java/controllers/MainController.java
+++ src/main/java/org/lessons/java/controllers/MainController.java
@@ -1,5 +1,11 @@
 package org.lessons.java.controllers;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.lessons.java.models.Movie;
+import org.lessons.java.models.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,5 +20,45 @@ public class MainController {
 		model.addAttribute("nome","Dario");
 		
 		return "index";
+	}
+	
+	private List<Movie> getBestMovies() {
+		List<Movie> bestMovies = new ArrayList<>();
+		bestMovies.add(new Movie(1,"Gattaca"));
+		bestMovies.add(new Movie(2,"Natural Born Killers"));
+		bestMovies.add(new Movie(3,"Quei Bravi Ragazzi"));
+		
+		return bestMovies;
+	}
+	
+	private List<Song> getBestSongs() {
+		List<Song> bestSongs = new ArrayList<>();
+		bestSongs.add(new Song(1,"Beat It"));
+		bestSongs.add(new Song(1,"Volare"));
+		bestSongs.add(new Song(1,"Ormai è tardi"));
+		
+		return bestSongs;
+	}
+	
+	@GetMapping("/movies")
+	public String getMovies(Model movies) {
+		String moviesToString = "";
+		for (Movie movie : this.getBestMovies()) {
+			moviesToString += movie + ", ";
+		}
+		movies.addAttribute("movies", moviesToString.substring(0, moviesToString.length() -2));
+		return "movies";
+	}
+	
+	@GetMapping("/songs")
+	public String getSongs(Model songs) {
+		String songsToString = "";
+		
+		for (Song song : this.getBestSongs()) {
+			songsToString += song + ", ";
+		}
+		songsToString = songsToString.substring(0, songsToString.length() -2);
+		songs.addAttribute("songs", songsToString);
+		return "songs";
 	}
 }

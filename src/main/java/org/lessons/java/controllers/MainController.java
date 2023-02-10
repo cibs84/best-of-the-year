63999651791e19a61a6f1324c726825d9b9666c4
cibs84 <d.tilesi@gmail.com>
2023-02-10 16:36:23 +0100

    Step 1 completato

---
+++ src/main/java/org/lessons/java/controllers/MainController.java
@@ -0,0 +1,18 @@
+package org.lessons.java.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+	
+	@GetMapping()
+	public String home(Model model) {
+		model.addAttribute("nome","Dario");
+		
+		return "index";
+	}
+}
